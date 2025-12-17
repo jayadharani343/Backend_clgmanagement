a01576daff99fdc0079f79b5cf98c8239dd65403
jayadharani343 <jayadharani.m2024aids@sece.ac.in>
2025-12-17 21:37:18 +0530

    Delete Routers directory

--- Routers/contactRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { sendMessage } = require("../Controllers/contactController");
-
-router.post("/contact", sendMessage);
-
-module.exports = router;
